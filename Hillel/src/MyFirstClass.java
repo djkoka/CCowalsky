2c252caf3336f9ca97e9cbbaf0dab7d7825add7b
CCowalsky <zuka321@gmail.com>
2015-09-17 21:19:40 +0300

    First Lessons(hello world)

---
+++ Hillel/src/MyFirstClass.java
@@ -0,0 +1,22 @@
+
+public class MyFirstClass {
+
+
+    public static void main (String[] args){
+
+        String hm = "Hello world";
+        System.out.println(hm);
+
+
+        final int age = 23;
+
+        System.out.println ("I`m "+age+" age");
+
+        System.out.println ("31"+ 31);
+        System.out.println (31+ 31);
+
+        //17.сент€брь
+
+    }
+
+}
