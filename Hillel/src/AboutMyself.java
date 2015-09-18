0b0b9c2a3139522d49bc7e1c716cb89ce43a8b57
djkoka <zuka321@gmail.com>
2015-09-18 07:45:36 +0300

    homework 1.

---
+++ Hillel/src/AboutMyself.java
@@ -0,0 +1,24 @@
+/**
+ * Created by djkoka on 18.09.2015.
+ */
+public class AboutMyself {
+
+    public static void main (String[] args) {
+
+        String firstName = "Constantin";
+        String lastName = "Kowalsky";
+
+        int thisYear = 2015;
+        int birthYear = 1992;
+        int age;
+
+
+        age = thisYear - birthYear;
+
+        System.out.println(firstName + "\u0020" + lastName);
+        System.out.println(firstName + " " + lastName);
+        System.out.println(age);
+
+
+    }
+}
