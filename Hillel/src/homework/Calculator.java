7cbb475a2396320584e96135368123a04bf097e8
djkoka <zuka321@gmail.com>
2015-09-30 22:15:49 +0300

    calc no 2 ex

--- Hillel/src/homework/Calculator.java
+++ Hillel/src/homework/Calculator.java
@@ -41,25 +41,32 @@ public class Calculator {
                 break;
             case "fract":
                 trunk = (int) a;
-                double fract = (double) a - trunk;
+                double fract = a - (int)a;
                 System.out.println("Result: " + fract);
                 break;
             default:
                 System.out.println("Invalid operator");
 
         }
+
     }
 
+
     public static void dataType(double result) {
-        if (result >= Byte.MIN_VALUE & result <= Byte.MAX_VALUE){
+        if (result - (int)result != 0){
+            System.out.println(" double");
+        }else if (result >= Byte.MIN_VALUE && result <= Byte.MAX_VALUE){
             System.out.print(" byte");
-        }else if (result >=Short.MIN_VALUE  & result <=Short.MAX_VALUE ){
+        }else if (result >=Short.MIN_VALUE  && result <=Short.MAX_VALUE ){
             System.out.print(" short");
-        }else if (result >=Integer.MIN_VALUE & result <= Integer.MAX_VALUE){
+        }else if (result >=Integer.MIN_VALUE && result <= Integer.MAX_VALUE){
             System.out.print(" integer");
-        }else if (result >=Long.MIN_VALUE & result <=Long.MAX_VALUE){
+        }else if (result >=Long.MIN_VALUE && result <=Long.MAX_VALUE){
             System.out.print(" long");
         }
+
+
+
     }
 
     public static double getB(Scanner scanner) {
