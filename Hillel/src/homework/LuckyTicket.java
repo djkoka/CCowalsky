d63c5ff177a5b406e8955bc906b3af0e71121fd9
djkoka <zuka321@gmail.com>
2015-10-04 00:32:33 +0300

    3

---
+++ Hillel/src/homework/LuckyTicket.java
@@ -0,0 +1,20 @@
+package homework;
+
+
+public class LuckyTicket {
+    public static void main(String[] args) {
+        int counter = 0;
+        for (int i = 1001; i < 1000000; i++) {
+            int number1 = i / 100000 % 10,
+                    number2 = i / 10000 % 10,
+                    number3 = i / 1000 % 10,
+                    number4 = i / 100 % 10,
+                    number5 = i / 10 % 10,
+                    number6 = i % 10;
+            if ((number1 + number2 + number3) == (number4 + number5 + number6)) {
+                counter++;
+            }
+        }
+        System.out.println("You need: " + counter+" gifts;");
+    }
+}
---
+++ Hillel/src/homework/Telegraph.java
@@ -0,0 +1,15 @@
+package homework;
+
+/**
+ * Created by djkoka on 02.10.2015.
+ */
+public class Telegraph {
+}
+
+/*
+        Напечатать квитанцию об оплате за телеграмму, если известна стоимость одного слова.
+     Примечание:
+    Разбить предложения на составные слова можно с помощью метода split()
+    String message = "hello world";
+    String[] words = message.split(" ");
+*/
