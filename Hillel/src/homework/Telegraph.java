3138a98902e7b05326f67bd2836a841a577f64d9
djkoka <zuka321@gmail.com>
2015-10-04 19:02:43 +0300

    3

--- Hillel/src/homework/Telegraph.java
+++ Hillel/src/homework/Telegraph.java
@@ -1,15 +1,20 @@
 package homework;
 
+import java.util.Scanner;
+
 /**
  * Created by djkoka on 02.10.2015.
  */
 public class Telegraph {
-}
 
-/*
-        Напечатать квитанцию об оплате за телеграмму, если известна стоимость одного слова.
-     Примечание:
-    Разбить предложения на составные слова можно с помощью метода split()
-    String message = "hello world";
-    String[] words = message.split(" ");
-*/
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String message = scanner.next();
+        double cost = 7.5;
+        String message = "Hello world!";
+        String[] words = message.split("");
+        System.out.println("In the message: "+words.length+" symbols.");
+        System.out.println("The cost of paying for a telegram: " +
+                +words.length*cost+" tugricov");
+    }
+}
