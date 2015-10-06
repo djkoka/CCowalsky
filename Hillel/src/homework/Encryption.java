11efdd48b9fa4ee1dedecf38b0e3c2b1e1924773
djkoka <zuka321@gmail.com>
2015-10-06 18:23:12 +0300

    commit

--- Hillel/src/homework/Encryption.java
+++ Hillel/src/homework/Encryption.java
@@ -1,43 +1,12 @@
 package homework;
 
-import java.util.Scanner;
 
 /**
  * Created by djkoka on 03.10.2015.
  */
 public class Encryption {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        System.out.print("Enter string: ");
-        String src = in.nextLine();
-
-        System.out.print("Enter key (int): ");
-        int key = in.nextInt();
-
-        System.out.format("\nSource string:\t%s\n", src);
-
-        String encrypted = encrypt(src, key);
-        System.out.format("Encrypted string:\t%s\n", encrypted);
-
-        String decrypted = decrypt(encrypted, key);
-        System.out.format("Decrypted string:\t%s\n\n", decrypted);
-    }
-
-    private static String encrypt(String src, int key) {
-        return shiftChars(src, key);
-    }
-
-    private static String decrypt(String src, int key) {
-        return shiftChars(src, -key);
-    }
-
-    private static String shiftChars(String src, int posCount) {
-        StringBuilder result = new StringBuilder();
-        for (char ch : src.toCharArray()) {
-            result.append(Character.toString((char)(ch + posCount)));
-        }
-        return result.toString();
     }
 }
 /*
