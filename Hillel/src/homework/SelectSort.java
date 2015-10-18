9ce0ed6855e18e63e28ddf0f7c93f1957c423a66
djkoka <zuka321@gmail.com>
2015-10-18 13:33:58 +0300

    hm

---
+++ Hillel/src/homework/SelectSort.java
@@ -0,0 +1,36 @@
+package homework;
+
+/**
+ * Created by djkoka on 18.10.2015.
+ */
+public class SelectSort {
+    public void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i];
+            int min_i = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    min_i = j;
+                }
+            }
+            if (i != min_i) {
+                int tmp = arr[i];
+                arr[i] = arr[min_i];
+                arr[min_i] = tmp;
+            }
+        }
+    }
+
+    public void bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+}
---
+++ Hillel/src/homework/Sort.java
@@ -0,0 +1,36 @@
+package homework;
+
+import java.util.Arrays;
+
+/**
+ * Created by djkoka on 17.10.2015.
+ */
+public class Sort {
+    public static void main(String[] args) {
+
+        int[] ints = new int[15];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = (int) (Math.random() * 100);
+            System.out.print(ints[i] + " ");
+        }
+        System.out.println("\n");
+        Arrays.sort(ints);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.print(ints[i] + " ");
+        }
+        System.out.println("\n");
+
+        SelectSort sort = new SelectSort();
+        sort.bubbleSort(ints);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.print(ints[i] + " ");
+        }
+        System.out.println("\n");
+
+        sort.selectionSort(ints);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.print(ints[i] + " ");
+        }
+        System.out.println("\n");
+    }
+}
