bb1ec1479a3d5503fcd667bb0a5cd2eca091394b
djkoka <zuka321@gmail.com>
2015-10-19 17:23:36 +0300

    hm

--- Hillel/src/homework/Sort.java
+++ Hillel/src/homework/Sort.java
@@ -20,12 +20,25 @@ public class Sort {
         }
         System.out.println("\n");
 
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = (int) (Math.random() * 100);
+            System.out.print(ints[i] + " ");
+        }
+        System.out.println("\n");
+
         SelectSort sort = new SelectSort();
         sort.bubbleSort(ints);
         for (int i = 0; i < ints.length; i++) {
             System.out.print(ints[i] + " ");
         }
         System.out.println("\n");
+
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = (int) (Math.random() * 100);
+            System.out.print(ints[i] + " ");
+        }
+        System.out.println("\n");
+
 
         sort.selectionSort(ints);
         for (int i = 0; i < ints.length; i++) {
